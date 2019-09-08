35fb43306a4c6b5a08ce05d8984f8357ad54b1fd
thanh.pham <canga.pham@gmail.com>
2019-09-08 20:56:48 +0200

    Finish havel-hakimi algorithm

--- havel_hakimi.py
+++ havel_hakimi.py
@@ -5,42 +5,56 @@ ACQUAINTANCE_LIST = [5, 3, 0, 2, 6, 2, 0, 7, 2, 5]
 
 def havel_hakimi(list):
     """
+    Havel-hakimi algorithm answers if the given list is a degree sequence of any simple graph. Degree sequence is a list
+     of neighbors of all vertices in a graph.
+
     Algorithm:
 
-    1. If the sequence is now empty (no elements left), stop and return true.
+    1. Remove all 0's from the sequence (i.e. warmup1).
 
-    2. Sort the sequence in descending order (i.e. warmup2).
+    2. If the sequence is now empty (no elements left), stop and return true.
 
-    3. Remove the first answer (which is also the largest answer, or tied for the largest) from the sequence and call
+    3. Sort the sequence in descending order (i.e. warmup2).
+
+    4. Remove the first answer (which is also the largest answer, or tied for the largest) from the sequence and call
     it N. The sequence is now 1 shorter than it was after the previous step.
 
-    4. If N is greater than the length of this new sequence (i.e. warmup3), stop and return false.
+    5. If N is greater than the length of this new sequence (i.e. warmup3), stop and return false.
 
-    5. Subtract 1 from each of the first N elements of the new sequence (i.e. warmup4).
+    6. Subtract 1 from each of the first N elements of the new sequence (i.e. warmup4).
 
-    6. Continue from step 1 using the sequence from the previous step.
+    7. Continue from step 1 using the sequence from the previous step.
 
     :param list: number of acquaintances in a group
     :return: True, if List is according to havel-hakimi algorithm
-            False, is it's not
+            False, if it's not
     """
+    while True:
+        print(list)
 
-    # 1. removing zeros
-    if list.count(0) == len(list):
-        return True
-    for i in range(list.count(0)):
-        list.remove(0)
-    # 2. sort list descending
-    list.sort(reverse = True)
+        # 1. removing zeros
+        if list.count(0) == len(list):
+            return True
+        for i in range(list.count(0)):
+            list.remove(0)
 
-    # 3. remove the first answer
-    n = list[0]
-    list.remove(n)
+        # 2. list is empty => return true
+        if len(list) == 0:
+            return True
 
-    # 4.  If N is greater than the length of this new sequence (i.e. warmup3), stop and return false.
-    if n > len(list):
-        return False
+        # 3. sort list descending
+        list.sort(reverse = True)
 
+        # 4. remove the first answer
+        n = list[0]
+        list.remove(n)
+
+        # 5.  If N is greater than the length of this new sequence (i.e. warmup3), stop and return false.
+        if n > len(list):
+            return False
+
+        # 6. Subtract 1 from each of the first N elements of the new sequence (i.e. warmup4).
+        list[:] = [x - 1 for x in list]
 
 
 
